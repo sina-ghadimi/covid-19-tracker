43266ab6fd6964c8ea60e8b561f6d056f4f80b39
sina ghadimi <sinaghadimi92@gmail.com>
2022-10-02 11:14:27 +0330

    add changeable URL to api and add CountrySelect to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import Cards from "./components/Cards";
 import { Box } from "@mui/material";
 import getCovidData from "./apis/getCovidData";
-
+import CoutrySelect from "./components/CoutrySelect";
 const App = () => {
   const [data, setData] = useState([]);
+  const [country, setCountry] = useState("iran");
+  const URL = country === "all" ? `/all` : `/countries/${country}`;
 
   useEffect(() => {
     const getData = async () => {
@@ -14,9 +16,30 @@ const App = () => {
     getData();
   }, [URL]);
 
+  const handleCountryChange = async country => {
+    const { data } = await getCovidData.get(URL);
+    setData(data);
+    setCountry(country);
+  };
+
   return (
-    <Box>
+    <Box
+      my={5}
+      mx={3}
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        gap: "36px",
+      }}
+    >
       <Cards data={data} />
+      <CoutrySelect
+        data={data}
+        country={country}
+        handleCountryChange={handleCountryChange}
+      />
     </Box>
   );
 };
