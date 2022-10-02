523aacf8629a0b629c89823770049ed1412b09af
sina ghadimi <sinaghadimi92@gmail.com>
2022-10-02 11:07:57 +0330

    implement CountrySelect Component

---
+++ src/components/CoutrySelect.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { FormControl, NativeSelect } from "@mui/material";
+import getCovidData from "../api/getCovidData";
+
+const CoutrySelect = ({ handleCountryChange }) => {
+  const [countriesInfo, setCountriesInfo] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await getCovidData.get("/countries");
+      setCountriesInfo(data);
+    };
+    getData();
+  }, []);
+  return (
+    <FormControl fullWidth>
+      <NativeSelect
+        defaultValue=""
+        onChange={e => handleCountryChange(e.target.value)}
+      >
+        <option value="all">Worldwide</option>
+        {countriesInfo.map(({ country }, index) => {
+          return <option key={index}>{country}</option>;
+        })}
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default CoutrySelect;
